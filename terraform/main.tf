776892b39daa89e795f228a5b6f02ebbe5a9b0de
sai127001 <codesai127.0.0.1@gmail.com>
2024-12-27 18:14:18 +0530

    Updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,47 +8,49 @@ resource "aws_ecr_repository" "saitechnicaltask" {
 
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_execution_role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
         Effect = "Allow"
-        Sid    = ""
         Principal = {
           Service = "lambda.amazonaws.com"
         }
       }
     ]
   })
-
-  inline_policy {
-    name   = "lambda_policy"
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action = [
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:PutLogEvents"
-          ]
-          Effect   = "Allow"
-          Resource = "arn:aws:logs:*:*:*"
-        },
-        {
-          Action = [
-            "s3:GetObject",
-            "rds:*",
-            "glue:*"
-          ]
-          Effect   = "Allow"
-          Resource = "*"
-        }
-      ]
-    })
-  }
 }
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name   = "lambda_policy"
+  role   = aws_iam_role.lambda_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Action = [
+          "s3:GetObject",
+          "rds:*",
+          "glue:*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 
 resource "aws_lambda_function" "saitechnicaltask" {
   function_name = "saitechnicaltask"
