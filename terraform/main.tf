9bd2b779e66869a918939bfad1ba635b31fd7498
sai127001 <codesai127.0.0.1@gmail.com>
2024-12-27 19:09:15 +0530

    Updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,7 +53,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
 
 
 resource "aws_lambda_function" "saitechnicaltask" {
-  function_name = "saitechnicaltask"
+  function_name = "saitechnicaltask1"
   role          = aws_iam_role.lambda_role.arn
   package_type  = "Image"
   image_uri     = "${aws_ecr_repository.saitechnicaltask.repository_url}:latest"
