c2146a0d9f9e6079bcbd71239b2b9f2aacae1825
sai127001 <codesai127.0.0.1@gmail.com>
2024-12-27 19:12:30 +0530

    Updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_ecr_repository" "saitechnicaltask" {
+resource "aws_ecr_repository" "saitechnicaltask1" {
   name = "saitechnicaltask1"
 }
 
@@ -52,14 +52,14 @@ resource "aws_iam_role_policy" "lambda_policy" {
 }
 
 
-resource "aws_lambda_function" "saitechnicaltask" {
+resource "aws_lambda_function" "saitechnicaltask1" {
   function_name = "saitechnicaltask1"
   role          = aws_iam_role.lambda_role.arn
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.saitechnicaltask.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.saitechnicaltask1.repository_url}:latest"
   timeout       = 60
 }
 
 output "lambda_function_name" {
-  value = aws_lambda_function.saitechnicaltask.function_name
+  value = aws_lambda_function.saitechnicaltask1.function_name
 }
